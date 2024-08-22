1514cf39c6cf42bc7aaf06ec882679f887e6dea7
ruuuu <rufinka_91@mail.ru>
2024-08-22 08:59:59 +0300

    add sending Order

--- src/modules/Order/Order.jsx
+++ src/modules/Order/Order.jsx
@@ -1,6 +1,6 @@
 import './order.scss';
 import { useSelector, useDispatch } from 'react-redux';
-import { closeModal } from '../../redux/orderSlice.js';
+import { closeModal, sendOrder } from '../../redux/orderSlice.js';
 import { useEffect } from 'react';
 import { updateOrderData } from '../../redux/orderSlice.js';
 
@@ -12,13 +12,11 @@ export const Order = () => {
 
     const dispatch = useDispatch();
     const isOrderReady = false; // если еще заказа нет
-    const orderId = useSelector((state) => { 
-        return state.order.isOrder;
-    });
+    const orderId = useSelector((state) => state.order.isOrder);
+    const isOpenModal = useSelector((state) => state.order.isOpenModal);
+    const orderData = useSelector((state) => state.order.data);
+    const itemsCart = useSelector((state) => state.cart.items);  // список товаров Корзины [{ id, photoUrl, name, price, quantity }, {}]
 
-    const isOpenModal = useSelector((state) => { 
-        return state.order.isOpenModal;
-    });
 
     // либо так написать дестуктрируя:
     // const { isOpenModal } = useSelector((state) => { 
@@ -43,10 +41,15 @@ export const Order = () => {
 
 
     const handleChange = (evt) => { // отправка данных заказа
-        const { name, value } = evt.target;                  // деструткирировали, <input name="" value="">
+        const { name, value } = evt.target;                  // деструткирировали, evt.target то <input name="" value="">
         dispatch(updateOrderData({ [name]: value}));        // [name] значит что туда заносится значение  атрибута name
     };
 
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        dispatch(sendOrder())
+    };
 
 
     useEffect(() => {
@@ -83,52 +86,52 @@ export const Order = () => {
                     (
                         <>
                             <h2 className="order__title"> Заказ офорлмен </h2> 
-                            <p className="order__id">Номер ваего заказа: {orderId} </p>  
+                            <p className="order__id"> Номер ваего заказа: {orderId} </p>  
                         </>
                     ) : 
                     (
                         <>
                             <h2 className="order__title"> Оформить заказ </h2> 
 
-                            <form className="order__form" id="order">       {/* id= нужен для связки фрмы и кноп Отправить: */}
+                            <form className="order__form" id="order" onSubmit={handleSubmit}>       {/* id= нужен для связки фрмы и кноп Отправить: */}
                                 <fieldset className="order__fieldset">
                                         <legend className="order__legend"> Данные заказчика </legend>
                                         <div className="order__input-group">
-                                            <input className="order__input" type="text" name="buyerName" placeholder="Имя" />
-                                            <input className="order__input" type="tel" name="buyerPhone" placeholder="Телефон" />
+                                            <input className="order__input" type="text" name="buyerName" value={orderData.buyerName} placeholder="Имя" onChange={handleChange} />   {/* при вводе текста в поле, сработает onChange. */}
+                                            <input className="order__input" type="tel" name="buyerPhone" value={orderData.buyerPhone}  placeholder="Телефон" onChange={handleChange} />
                                         </div>
                                 </fieldset> 
 
                                 <fieldset className="order__fieldset">
                                         <legend className="order__legend"> Данные получателя </legend>
                                         <div className="order__input-group">
-                                            <input className="order__input" type="text" name="recipientName" placeholder="Имя" />
-                                            <input className="order__input" type="tel" name="recipientPhone" placeholder="Телефон" />
+                                            <input className="order__input" type="text" name="recipientName" value={orderData.recipientName} placeholder="Имя" onChange={handleChange} />
+                                            <input className="order__input" type="tel" name="recipientPhone" value={orderData.recipientPhone} placeholder="Телефон" onChange={handleChange}  />
                                         </div>
                                 </fieldset> 
 
                                 <fieldset className="order__fieldset">
                                     <legend className="order__legend"> Адрес </legend>
                                     <div className="order__input-group">
-                                        <input className="order__input" type="text" name="street" placeholder="Улица" />
-                                        <input className="order__input order__input--min" type="number" name="house" placeholder="Дом" />
-                                        <input className="order__input order__input--min" type="number" name="apartment" placeholder="Квартира" />
+                                        <input className="order__input" type="text" name="street" placeholder="Улица" onChange={handleChange} />
+                                        <input className="order__input order__input--min" type="number" name="house" value={orderData.house} placeholder="Дом" onChange={handleChange} />
+                                        <input className="order__input order__input--min" type="number" name="apartment" value={orderData.apartment} placeholder="Квартира" onChange={handleChange} />
                                     </div>
                                 </fieldset> 
 
                                 <fieldset className="order__fieldset"> 
                                     <div className="order__payment">
                                         <label className="order__label-radio">          {/* атрибут for не нужен тк поле внутри label: */}
-                                            <input className="order__radio" type="radio" name="paymentOnline" value="true" defaultChecked /> Оплата онлайн  {/* вместо cheked=true пишем defaultChecked */}
+                                            <input className="order__radio" type="radio" name="paymentOnline" value={orderData.paymentOnline === "true"}  defaultChecked onChange={handleChange} /> Оплата онлайн  {/* вместо cheked=true пишем defaultChecked */}
                                         </label>
                                     </div> 
 
                                     <div className="order__delivery">
                                         <label htmlFor="delivery"> Доставка завтра </label>  {/* вместо атрбиута for ставим  htmlFor  */}
-                                        <input type="hidden" name="deliveryDate" value="10.05" />  {/* скрытое поле:  */}
+                                        <input type="hidden" name="deliveryDate" value={orderData.deliveryDate}  />  {/* скрытое поле:  */}
 
                                         <div className="order__select-wrapper">
-                                            <select className="order__select" name="deliveryTime" id="delivery">   {/* id такое же что и у <label for="delivery">Доставка завтра</label>  */}
+                                            <select className="order__select" name="deliveryTime" value={orderData.deliveryTime} id="delivery" onChange={handleChange}>   {/* id такое же что и у <label for="delivery">Доставка завтра</label>  */}
                                                 <option value="9-12">с 9:00 до 12:00</option>
                                                 <option value="12-15">с 12:00 до 15:00</option>
                                                 <option value="15-18">с 15:00 до 18:00</option>
@@ -140,7 +143,12 @@ export const Order = () => {
                             </form> 
 
                             <div className="order__footer">
-                                <p className="order__total-price"> 90000 Р </p> 
+                                <p className="order__total-price"> 
+                                    {
+                                        itemsCart.reduce((acc, item) => {
+                                            return acc+= item.quantity * item.price
+                                    }, 0) }&nbsp;₽ 
+                                </p> 
                                 <button className="order__button" type="submit" form="order"> Заказать </button>     {/* у атрибута form значение такое же чтои  у form.id, тогда при нажатии на Заказать,форма отправится:*/}
                             </div>
                         </> 
--- src/redux/orderSlice.js
+++ src/redux/orderSlice.js
@@ -1,4 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+
+// Отправка заказа
+//                                                                        thunkAPI 
+export const sendOrder = createAsyncThunk('order/sendOrder', async(_, { getState, dispatch }) => { // отправка заказа на сервер, деструктурировали thunkAPI
+  
+  const state = getState();
+  const orderData = {
+    "buyer": {
+      "name": state.order.data.buyerName,
+      "phone": state.order.data.buyerPhone,
+    },
+    "recipient": {
+      "name": state.order.data.recipientName,
+      "phone": state.order.data.recipientPhone,
+    },
+    "address": `${state.order.data.street},  ${state.order.data.house}, ${state.order.data.apartment}`,
+    "paymentOnline": state.order.data.paymentOnline,
+    "deliveryDate": state.order.data.deliveryDate,
+    "deliveryTime": state.order.data.deliveryTime,
+  };
+
+  const response = await fetch(`${API_URL}/api/orders`, {
+    method: 'POST', 
+    credentials: 'include', // куки вклбчены
+    headers: {
+      "Content-Type": 'application/json',
+    },
+    body: JSON.stringify(orderData),
+  });
+
+  if(!response.ok){
+    throw new Error("не можем отпраить заказ на сервер");
+  }
+
+  if(response.ok){
+    dispatch(clearOrder());
+    dispatch(toggleCart()); // закрыли корзину
+    dispatch(fetchCart()); // получение  корзины
+  }
+
+  return await response.json();
+});
+
 
 
 
@@ -26,7 +70,7 @@ const initialState = {
 const orderSlice = createSlice({
   name: 'order', // нзв стейта сами придумали
   initialState,
-  reducers: {  // редьюсер - это фукния которая меняет состояния
+  reducers: {  // редьюсер - это фукния которая меняет состояние
     
     openModal(state){   // openModal-редьюсер(action), state =  { isOpenModal: false, }
       state.isOpenModal = true;
@@ -36,10 +80,10 @@ const orderSlice = createSlice({
       state.isOpenModal = false;
     },
 
-    clearOrder(state){ // редьюсер
+    clearOrder(state){ // редьюсер, после отправки заказ очищаем форму
 
       state.data = {
-        buyerName:  '',
+        buyerName:  '', // эти значения свйства взяли у name текстовых полей
         buyerPhone: '',
         recipientName: '',
         recipientPhone: '',
@@ -50,17 +94,32 @@ const orderSlice = createSlice({
         deliveryDate: '',
         deliveryTime:'',
       }
-      
     },
 
     updateOrderData(state, action){  // редьюсер
-      state.data[action.payload.name] = action.payload.value;   // { name, value } значения котрые введем в  текстовое поле
+      state.data[action.payload.name] = action.payload.value;   // { name, value } у поля,  value - значение котрые введем в поле <input name="" value="">
      // либо так:
      // state.data = { ...state.data, ...action.payload }
     },
+  },
 
+  extraReducers: (builder) => {
 
-  }
+    builder.addCase(sendOrder.pending, (state) => {
+      state.status = 'loading';  // loading сами придумали, ждем ответа от сервера
+      state.categories = [];
+    })
+    builder.addCase(sendOrder.fulfilled, (state, action) => {
+      state.status = 'succeeded';  // succeeded сами придумали, сервер ответил
+      state.items = action.payload; // в action.payload будет то, что сервер отдаст 
+      
+      
+    })
+    builder.addCase(sendOrder.rejected, (state, action) => {
+      state.status = 'failed';  // ошибка при запросе сервера
+      state.error = action.error.message;
+    });
+  },  
   
 
 });
